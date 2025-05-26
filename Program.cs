4499c10f7e3bd7d9d128265e55051f86c752c6da
Marat <maratf76@gmail.com>
2025-05-26 16:25:10 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string path = "note"+DateTime.Now.ToString("yyyyMMdd")+".txt";            
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+
+
+            // добавление в файл
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                await writer.WriteAsync("User: "+userName+"  ");
+                await writer.WriteLineAsync(args[0]+" "+DateTime.Now.ToLongTimeString());                
+            }
+        }
+    }
+}
