60e6f6db6be12445b49137bb550f8b99a7e50261
Marat <maratf76@gmail.com>
2025-05-30 11:48:16 +0500

    2025 05 30 0011

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ConsoleApp1
 {
@@ -13,7 +12,7 @@ namespace ConsoleApp1
         {
             string path = "note"+DateTime.Now.ToString("yyyyMMdd")+".txt";            
             string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-
+            
 
             // добавление в файл
             using (StreamWriter writer = new StreamWriter(path, true))
@@ -21,6 +20,35 @@ namespace ConsoleApp1
                 await writer.WriteAsync("User: "+userName+"  ");
                 await writer.WriteLineAsync(args[0]+" "+DateTime.Now.ToLongTimeString());                
             }
+
+
+            EventLog eventLog = new EventLog();
+
+            eventLog.Source = "Visual Studio";
+            //eventLog.Log = "Application";
+            eventLog.Log = "";
+
+            try
+            {
+                // Ваш код здесь
+
+                //  int h = 10;
+
+                //  h -= 10;
+                //  int result = 10 / h; // Искусственная ошибка
+                string t1 = "User: " + userName + "  " + args[0] + " " + DateTime.Now.ToLongTimeString();
+                eventLog.WriteEntry(t1, EventLogEntryType.Information);
+            }
+            catch (Exception ex)
+            {
+                eventLog.WriteEntry("Ошибка: " + ex.Message, EventLogEntryType.Error);
+            }
+            finally
+            {
+                eventLog.Close();
+            }
+
+
         }
     }
 }
